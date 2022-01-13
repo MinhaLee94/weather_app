df45c09ea8976952bf1e77ba018ba3e5e8a27272
MinhaLee94 <shjune1994@gmail.com>
2022-01-12 23:16:32 -0500

    Get location information

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Dimensions, StyleSheet, ScrollView } from "react-native";
+import * as Location from "expo-location";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
-export default function App() {
+const App = () => {
+	const [location, setLocation] = useState();
+	const [locationPermission, setLocationPermission] = useState(true);
+	const [city, setCity] = useState("Loading...");
+	const askPermission = async() => {
+		const { granted } = await Location.requestForegroundPermissionsAsync();
+		if (!granted) {
+			setLocationPermission(false);
+		}
+
+		const {
+			coords: { latitude, longitude },
+		} = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+		const location = await Location.reverseGeocodeAsync(
+			{ latitude, longitude },
+			{ useGoogleMaps: false }
+		);
+
+		setCity(location[0].city);
+	};
+
+	useEffect(() => {
+		askPermission();
+	}, []);
+
     return (
 		<View style={styles.container}>
 			<View style={styles.city}>
-				<Text style={styles.cityName}>Seoul</Text>
+				<Text style={styles.cityName}>{city}</Text>
 			</View>
 			<ScrollView
 				pagingEnabled
@@ -64,4 +90,6 @@ const styles = StyleSheet.create({
 	  marginTop: -30,
 	  fontSize: 60,
 	},
-  });
+});
+
+export default App;
